a647b3e2fac3e2443aa8e4911374e6c71fa7524f
Alex Movila <alex_movila@yahoo.com>
2024-05-27 08:54:59 +0300

    initial commit

--- src/main/java/com/alexm/wh/monitor/WarehouseMonitoringApp.java
+++ src/main/java/com/alexm/wh/monitor/WarehouseMonitoringApp.java
@@ -6,8 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
